bd28ec4b61058c1e7a641dbced9f035a32487260
my_name <my_email@gmail.com>
2019-08-04 19:19:24 +0300

    tasks

---
+++ test3.php
@@ -0,0 +1,41 @@
+<?php
+$min = 1;
+$max = 3;
+$a = rand($min ,$max );
+echo "    $a   ";
+if($a=1){
+class Circle{
+function circl()
+{
+$min1=0;
+$max1=200;
+$rad = rand($min1,$max1);
+$pl1=3.14*$rad;
+return($pl1);
+}
+}
+$obj = new Circle();
+echo($obj->circl());
+}
+else if($a=2){
+class Piramida{
+public function pir()
+{
+$min2=0;
+$max2=200;
+$lng=rand ($min2,$max2);
+$pl2=$l*$l;
+}}}
+else if($a=3){
+class Pryamoyg{
+public function Pr(){
+{
+$min3=0;
+$max3=200;
+$min4=0;
+$max4=200;
+$b=rand ($min3,$max3);
+$c=rand ($min4,$max4);
+$pl3=$b*$c;
+}}}}
+?>
